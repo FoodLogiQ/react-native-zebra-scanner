b1dfe5fa215a42500bbdbecd11845df9b1202174
Jokotoye Ademola <jokotoyeademola95@gmail.com>
2025-02-13 21:19:50 +0100

    chore: update repo with previous Rene changes

---
+++ index.js
@@ -0,0 +1,39 @@
+const ReactNative = require('react-native');
+const { NativeModules, DeviceEventEmitter } = ReactNative;
+const ZebraScanner = NativeModules.ZebraScanner || {}; // Hacky fallback for iOS
+
+const allowedEvents = [
+  ZebraScanner.BARCODE_READ_SUCCESS,
+  ZebraScanner.BARCODE_READ_FAIL,
+];
+
+let listener = null;
+
+/**
+ * Listen for available events
+ * @param  {String} eventName Name of event one of barcodeReadSuccess, barcodeReadFail
+ * @param  {Function} handler Event handler
+ */
+ ZebraScanner.on = (eventName, handler) => {
+  if (!allowedEvents.includes(eventName)) {
+    throw new Error(`Event name ${eventName} is not a supported event.`);
+  }
+  listener = DeviceEventEmitter.addListener(eventName, handler);
+};
+
+/**
+ * Stop listening for event
+ * @param  {String} eventName Name of event one of barcodeReadSuccess, barcodeReadFail
+ * @param  {Function} handler Event handler
+ */
+ ZebraScanner.off = (eventName, handler) => {
+  if (!allowedEvents.includes(eventName)) {
+    throw new Error(`Event name ${eventName} is not a supported event.`);
+  }
+  
+  if (listener) {
+    listener.remove();
+  }
+};
+
+module.exports = ZebraScanner;
